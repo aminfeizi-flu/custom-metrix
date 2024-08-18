ac584aa54c324bd239b12701bfa1f52d38baa080
Salman Qadami <salmanqadami@gmail.com>
2024-08-18 22:04:38 +0330

    update SDK plugin to v2.2.8

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
-import 'package:metrix_plugin/Metrix.dart';
-import 'package:metrix_plugin/MetrixAttribution.dart';
 import 'package:flutter/material.dart';
+import 'package:metrix_analytics/Metrix.dart';
+import 'package:metrix_analytics/MetrixAttribution.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String sessionNum = "";
+  String sessionNumber = "";
   String sessionId = "";
   String userId = "";
   String deeplink = "";
@@ -23,7 +25,7 @@ class _MyAppState extends State<MyApp> {
     super.initState();
 
     setState(() {
-      sessionNum = "";
+      sessionNumber = "";
       sessionId = "";
       userId = "";
       deeplink = "";
@@ -31,35 +33,60 @@ class _MyAppState extends State<MyApp> {
     });
 
     // iOS only
-    Metrix.initialize("lcqmfsnvhzznvhe");
+    Metrix.initialize('lcqmfsnvhzznvhe');
 
-    Metrix.addUserAttributes({
-      "name": "hisName"
-    });
+    Metrix.setUserAttributes({"name": "hisName"});
+
+    Metrix.setUserCustomId("flutter"); // call when user tries to login in your system and set userId value that user already knows in your system
+    // Metrix.deleteUserCustomId(); // call when your user goes to logout in your system
+    Metrix.setUserFirstName("userFirstName");
+    Metrix.setUserLastName("userLastName");
+    Metrix.setUserPhoneNumber("phoneNumber");
+    Metrix.setUserHashedPhoneNumber("hashedPhoneNumber");
+    Metrix.setUserEmail("email");
+    Metrix.setUserHashedEmail("hashedEmail");
+    Metrix.setUserCountry("country");
+    Metrix.setUserCity("city");
+    Metrix.setUserRegion("region");
+    Metrix.setUserLocality("locality");
+    Metrix.setUserGender("MALE"); // gender value could be "MALE" , "FEMALE" or "OTHER"
+    Metrix.setUserBirthday(12312412); // birthday value type should be 'Long' timestamp
+    Metrix.userChannelEnabled("SMS"); // channel value could be "SMS", "PUSH" or "EMAIL"
+    Metrix.userChannelDisabled("EMAIL"); // channel value could be "SMS", "PUSH" or "EMAIL"
+    // Metrix.setPushToken("fcmToken");
 
     Metrix.getAttributionData().listen((value) => {
-      this.setState(() {
+      setState(() {
         attribution = value;
       })
     });
 
     Metrix.getUserId().listen((value) => {
-      this.setState(() {
+      setState(() {
         userId = value;
       })
     });
 
+    Metrix.getSessionNumber().listen((sessionNum) => {
+      setState(() {
+        sessionNumber = sessionNum.toString();
+      })
+    });
+
     Metrix.getSessionId().listen((id) => {
-      this.setState(() {
+      setState(() {
         sessionId = id;
       })
     });
 
-    Metrix.getSessionNumber().listen((sessionNumber) => {
-      this.setState(() {
-        sessionNum = sessionNumber.toString();
-      })
-    });
+    // iOS only
+    Metrix.setStore("AppStore");
+
+    // iOS only
+    Metrix.setAppSecret(1, 429751687, 1057026454, 796046595, 610423971);
+
+    // iOS only
+    Metrix.setDefaultTracker("uevt4h");
   }
 
   @override
@@ -71,49 +98,53 @@ class _MyAppState extends State<MyApp> {
           ),
           body: Center(child: Column(children: <Widget>[
             Container(
-                margin: EdgeInsets.all(20),
-                child: Text('Session number is: $sessionNum')
+                margin: const EdgeInsets.all(20),
+                child: Text('Session number is: $sessionNumber')
             ),
             Container(
-                margin: EdgeInsets.all(20),
+                margin: const EdgeInsets.all(20),
                 child: Text('Session id is: $sessionId')
             ),
             Container(
-              margin: EdgeInsets.all(20),
-              child: FlatButton(
-                child: Text('SendEvent'),
-                color: Colors.blueAccent,
-                textColor: Colors.white,
+              margin: const EdgeInsets.all(20),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.blueGrey,
+                ),
                 onPressed: () {
-                  Metrix.newEvent('lbuoa', {
-                    "name": "hisName"
-                  });
+                  Metrix.newEvent('lbuoa', {"first": "second"});
                   Metrix.newEvent('lbuoa');
                 },
+                child: const Text(
+                  'SendEvent',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ),
             Container(
-              margin: EdgeInsets.all(20),
-              child: FlatButton(
-                child: Text('SendRevenue'),
-                color: Colors.blueAccent,
-                textColor: Colors.white,
+              margin: const EdgeInsets.all(20),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.blueGrey,
+                ),
                 onPressed: () {
                   Metrix.newRevenue('ykwyp', 2500.5);
                   Metrix.newRevenue('ykwyp', 2500.5, currency: 0);
-                  Metrix.newRevenue('ykwyp', 2500.5, currency: 0, orderId: "someId");
-                  Metrix.newRevenue('ykwyp', 2500.5, orderId: "someId");
                 },
+                child: const Text(
+                  'SendRevenue',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ),
             Container(
-                margin: EdgeInsets.all(20),
-                child: Text('UserId is: $userId')
-            ),
+                margin: const EdgeInsets.all(20), child: Text('UserId is: $userId')),
             Container(
-                margin: EdgeInsets.all(20),
-                child: Text('User attribution status: ${attribution?.attributionStatus ?? ""}')
-            ),
+                margin: const EdgeInsets.all(20),
+                child: Text(
+                    'User attribution status: ${attribution?.attributionStatus ?? ""}')),
           ]))
       ),
     );
